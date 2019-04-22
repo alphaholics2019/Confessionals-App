73e2a741699d464bf6390d266a6cc6242040a41d
learn <learn@learn12-2.localdomain>
2019-04-22 09:10:10 -0700

    added validations to Confessions class

--- app/controllers/confessions_controller.rb
+++ app/controllers/confessions_controller.rb
@@ -3,49 +3,47 @@ class ConfessionsController < ApplicationController
       before_action :authenticate_user!
 
       def index
-        @confessions = Confession.all
+     confessions = Confession.all
 
-        render json: @confessions
-      end
+     render json: confessions
+   end
 
-      def show
-        render json: @confession
-      end
+   def show
+     render json: confession
+   end
 
-      def create
-        @confession = current_user.confessions.new(confession_params)
+   def create
+     confession = current_user.confessions.create(confession_params)
 
-        respond_to do |format|
-            if @confession.save
-              format.html { redirect_to @confession }
-              format.json { render :show, status: :created, location: @confession }
-            else
-              format.html { render :new }
-              format.json { render json: @confession.errors, status: :unprocessable_entity }
-            end
-          end
-      end
+     if confession.save
+       render json: confession
+     else
+       render json: confession.errors.full_messages
+     end
 
-      def update
-        if @confession.update(apartment_params)
-          render json: @confession
-        else
-          render json: @confession.errors, status: :unprocessable_entity
-        end
-      end
+   end
 
-      def destroy
-        @confession.destroy
-      end
+   def update
+     if confession.update(confession_params)
+       render json: confession
+     else
+       render json: confession.errors, status: :unprocessable_entity
+     end
+   end
 
-      private
-        # Use callbacks to share common setup or constraints between actions.
-        def set_confession
-          @confession = Confession.find(params[:id])
-        end
+   def destroy
+     confession.destroy
+   end
 
-        # Only allow a trusted parameter "white list" through.
-        def confession_params
-          params.require(:confession).permit(:user_id, :name, :gif_url)
-        end
+   private
+     # Use callbacks to share common setup or constraints between actions.
+     def set_confession
+       confession = Confession.find(params[:id])
+     end
+
+     # Only allow a trusted parameter "white list" through.
+     def confession_params
+       params.require(:confession).permit(:name, :gif_url)
+     end
+     
     end
